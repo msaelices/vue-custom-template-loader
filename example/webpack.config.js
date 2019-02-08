0392022b740f31f9efdb8180bcc020d1f4db5aa9
Manuel Saelices <msaelices@gmail.com>
2019-02-08 23:50:18 +0100

    Example (WIP)

---
+++ example/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, './main.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+  },
+  devServer: {
+    stats: "minimal",
+    contentBase: __dirname
+  },
+  module: {
+    rules: [
+      // { loader: require.resolve('./debugger') },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      },
+      // this rule applies to <template-native> blocks
+      {
+        resourceQuery: /blockType=template-native/,
+        loader: 'vue-custom-template-loader'
+      },
+    ]
+  },
+  resolveLoader: {
+    alias: {
+      'vue-custom-template-loader': require.resolve('../index')
+    }
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+}
